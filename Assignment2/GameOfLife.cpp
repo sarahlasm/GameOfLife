f5893bddace762689f989bac07f5c141d67a759b
agalemmo <galem100@mail.chapman.edu>
2017-09-27 02:32:58 -0700

    god i did a lot of things

--- Assignment2/GameOfLife.cpp
+++ Assignment2/GameOfLife.cpp
@@ -20,122 +20,176 @@
   {
     GameMode* newGame;
 
-    string mapOrFile;
-    double popDensity;
+    int checkpoints = 0;
+    bool inputCheck = false;
+    bool modeCheck = false;
+    bool enterCheck = false;
+    bool pauseCheck = false;
+    bool outputCheck = false;
+
+    string mapOrFile = "";
     int rows = 0;
     int cols = 0;
-    string fileName;
-    string mode;
-    int genCount;
-    string autoOrNo;
-    string pPause;
-    string optOutput;
-    string outputFile;
-    bool yOutput;
+    double popDensity = 0.0;
+    string fileName = "";
     string input;
-    ifstream inFile;
-    bool doPause = false;
-    bool toggleToggle;
+    string mode;
+    string autoOrNo;
+    bool enterToggle; //false = auto
+    string pauseOrNo;
+    bool pauseToggle;
+    string outputTo;
+    string outputFile;
+    bool outputToggle;
 
-    cout << "Do you want a random map file (r) or a file path (f)?" << endl;
-    cin >> mapOrFile;
-    if (mapOrFile == "r" || mapOrFile == "random")
-    {
-      cout << "Number of rows?" << endl;
-      cin >> rows;
-      cout << "Number of columns?" << endl;
-      cin >> cols;
-      cout << "Enter a decimal between 0 and 1 for population density." << endl;
-      cin >> popDensity;
-    }
-    else if (mapOrFile == "f" || mapOrFile == "file")
-    {
-      cout << "File path?" << endl;
-      cin >> fileName;
-      ifstream inFile(fileName);
-      if (getline(inFile, input)) rows = stoi(input);
-      if (getline(inFile, input)) cols = stoi(input);
-    }
-    cout << "What kind of mode do you want? Classic (c), doughnut (d), or mirror (m)?" << endl;
-    cin >> mode;
 
-    if (mode == "d" || mode == "doughnut")
+    while (checkpoints < 5)
     {
-      newGame = new Doughnut(rows, cols);
-    }
-    else if (mode == "m" || mode == "mirror")
-    {
-      newGame = new Mirror(rows, cols);
-    }
-    else
-    {
-      newGame = new Classic(rows, cols);
-    }
-
-    while (toggleToggle == NULL) //god i hope this works
-    {
-    	cout << "Do you want the simulation to run automatically (a) or for you to toggle (t) each generation using the enter key?" << endl;
-    	cin >> autoOrNo;
-
-      switch (autoOrNo) {
-        case "t":
-          toggleToggle = true;
+      while (!inputCheck)
+      {
+        cout << "Do you want a random map file (r) or a file path (f)?" << endl;
+        cin >> mapOrFile;
+        if (mapOrFile == "r" || mapOrFile == "random")
+        {
+          cout << "Number of rows?" << endl;
+          cin >> rows; //check this
+          cout << "Number of columns?" << endl;
+          cin >> cols; //check this
+          cout << "Enter a decimal between 0 and 1 for population density." << endl;
+          cin >> popDensity; //check this
+          ++checkpoints;
+          inputcheck = true;
           break;
-        case "toggle":
-          toggleToggle = true;
+        }
+        else if (mapOrFile == "f" || mapOrFile == "file")
+        {
+          cout << "File path?" << endl;
+          cin >> fileName;
+          ifstream inFile(fileName);
+          if (getline(inFile, input)) rows = stoi(input);
+          if (getline(inFile, input)) cols = stoi(input);
+          ++checkpoints;
+          inputcheck = true;
           break;
-        case "a":
-          toggleToggle = false;
-          break;
-        case "automatic":
-          toggleToggle = false;
-          break;
-        case "automatically":
-          toggleToggle = false;
-          break;
-        default:
-          cout << "Please enter a valid input" << endl;
-          break;
+        }
+        else
+        {
+          cout << "Please enter a valid input." << endl;
+          continue;
+        }
       }
-    }
+      while (!modeCheck)
+      {
+        cout << "What kind of mode do you want? Classic (c), doughnut (d), or mirror (m)?" << endl;
+        cin >> mode;
 
-    while (doPause == NULL)
-    {
-  		//ugh do a switch UGH
-  		cout << "Do you want a pause between each generation? (y/n)" << endl;
-  		cin >> pPause;
+        if (mode == "d" || mode == "doughnut")
+        {
+          newGame = new Doughnut(rows, cols);
+          ++checkpoints;
+          modeCheck = true;
+          break;
+        }
+        else if (mode == "m" || mode == "mirror")
+        {
+          newGame = new Mirror(rows, cols);
+          ++checkpoints;
+          modeCheck = true;
+          break;
+        }
+        else if (mode == "c" || mode == "classic")
+        {
+          newGame = new Classic(rows, cols);
+          ++checkpoints;
+          modeCheck = true;
+          break;
+        }
+        else
+        {
+          cout << "Please enter a valid input." << endl;
+          continue;
+        }
+      }
+      while (!enterCheck)
+      {
+        cout << "Would you like for the output to be automatic (a) or toggled (t)?" << endl;
+        cin >> autoOrNo;
 
-      switch (pPause) {
-        case "y":
-          doPause = true;
+        if (autoOrNo == "a" || autoOrNo == "auto" || autoOrNo == "automatic")
+        {
+          enterToggle = false;
+          ++checkpoints;
+          enterCheck = true;
           break;
-        case "yes":
-          doPause = true;
+        }
+        else if (autoOrNo == "t" || autoOrNo == "toggle")
+        {
+          enterToggle = true;
+          ++checkpoints;
+          enterCheck = true;
           break;
-        case "pause":
-          doPause = true;
-          break;
-        case "n":
-          doPause = false;
-          break;
-        case "no":
-          doPause = false;
-          break;
-        default:
-          cout << "Please enter a valid input" << endl;
+        }
+        else
+        {
+          cout << "Please enter valid input." << endl;
+          continue;
+        }
+      }
+      while (!pauseCheck)
+      {
+        if (!enterToggle)
+        {
+          cout << "Would you like to have a pause (p) between each generation?" << endl;
+          cin >> pauseOrNo;
+
+          if (pauseOrNo == "p" || pauseOrNo == "pause")
+          {
+            pauseToggle = true;
+            ++checkpoints;
+            pauseCheck = true;
+            break;
+          }
+          else
+          {
+            cout << "Please enter valid input." << endl;
+            continue;
+          }
+        }
+        else
+        {
+          ++checkpoints;
+          pauseToggle = false;
+          pauseCheck = true;
           break;
         }
       }
-
-  	cout << "Do you want to output the results to another file? (y/n)" << endl;
-  	cin >> optOutput;
-
-  	if (optOutput == "y" || optOutput == "yes")
-  	{
-  		cout << "What file name?" << endl;
-  		cin >> outputFile;
-  		yOutput = true;
-  	}
+      while (!outputCheck)
+      {
+        cout << "Would you like to output to a file? (y/n)" << endl;
+        cin >> outputTo;
+        if (outputTo == "y" || outputTo == "yes")
+        {
+          cout << "Name of file?" << endl;
+          cin >> outputfile;
+          ++checkpoints;
+          outputToggle = true;
+          outputCheck = true;
+          break;
+        }
+        else if (outputTo == "n" || outputTo == "no")
+        {
+          ++checkpoints;
+          outputToggle = false;
+          outputCheck = true;
+          break;
+        }
+        else
+        {
+          cout << "Please provide valid input." << endl;
+          continue;
+        }
+      }
+    }
 
     if (mapOrFile == "r" || mapOrFile == "random")
       (*newGame).setUpBoard(popDensity);
@@ -168,6 +222,6 @@
   		(*newGame).changeGrid();
   		++genCount;
   	}
-
     return 0;
+
   }
