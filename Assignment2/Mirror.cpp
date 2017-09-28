7caa62922c2674f79bb1320892d942e96583ed79
agalemmo <galem100@mail.chapman.edu>
2017-09-27 23:24:20 -0700

    fixed mirror
    this should work

--- Assignment2/Mirror.cpp
+++ Assignment2/Mirror.cpp
@@ -14,211 +14,257 @@ Mirror::~Mirror()
 
 int Mirror::checkNeighbors(int row, int col)
 {
+
   char** thisGen = getThisGen();
   int numNeighbors = 0;
 
-  if (row > 0 && row < GameMode::getRows() && col > 0 && col < GameMode::getCols())
+  if (row == 0)
   {
-     for (int i = row - 1; i < row + 2; ++i)
-     {
-        for (int j = col - 1; j < col + 2; ++j)
+    if (col == 0)
+    {
+      for (int r = 0; r < 2; ++r)
+      {
+        for (int c = 0; c < 2; ++c)
         {
-           if (i == row && j == col)
-              continue;
-           else if (GameMode::isOccupied(thisGen[i][j]))
+          if (r == row && c == col)
+            numNeighbors += 3;
+          else if (GameMode::isOccupied(thisGen[r][c]))
+          {
+            if (r == 0 || c == COLS - 1 || r == ROWS - 1 || c == 0)
+              numNeighbors += 2;
+            else
               ++numNeighbors;
+          }
         }
-     }
-  } //end center check
-
-
-else if (row == GameMode::getRows())
-{
-   if (col == 0)
-   {
-      numNeighbors += 2;
-      for (int i = row - 1; i < row + 1; ++i)
-      {
-         for (int j = col; j < col + 2; ++j)
-         {
-            if (i == row && j == col)
-               continue;
-            else if (GameMode::isOccupied(thisGen[i][j]))
-            {
-               if (i == 0 || j == 0 || i == GameMode::getRows() || j == GameMode::getCols())
-                  numNeighbors += 2;
-               else
-                  ++numNeighbors;
-            }
-         }
       }
-   }
-
-   else if (col == GameMode::getCols())
-   {
-      numNeighbors += 2;
-      for (int i = row - 1; i < row + 1; ++i)
-      {
-         for (int j = col - 1; j < col + 1; ++j)
-         {
-            if (i == row && j == col)
-               continue;
-            else if (GameMode::isOccupied(thisGen[i][j]))
-            {
-               if (i == 0 || j == 0 || i == GameMode::getRows() || j == GameMode::getCols())
-                  numNeighbors += 2;
-               else
-                  ++numNeighbors;
-            }
-         }
-      }
-   }
-
-   else
-   {
-      ++numNeighbors;
-      for (int i = row - 1; i < row + 1; ++i)
-      {
-         for (int j = col - 1; j < col + 2; ++j)
-         {
-            if (i == row && j == col)
-               continue;
-            else if (GameMode::isOccupied(thisGen[i][j]))
-            {
-               if (i == 0 || j == 0 || i == GameMode::getRows() || j == GameMode::getCols())
-                  numNeighbors += 2;
-               else
-                  ++numNeighbors;
-            }
-         }
-      }
-   }
-} //end row == GameMode::getRows()
-
-else if (col == 0)
-{
-    if (row == 0)
-    {
-        numNeighbors += 2;
-        for (int i = row; i < row + 2; ++i)
-        {
-            for (int j = col; j < col + 2; ++j)
-            {
-                if (i == row && j == col)
-                    continue;
-                else if (GameMode::isOccupied(thisGen[i][j]))
-                {
-                    if (i == 0 || j == 0 || i == GameMode::getRows() || j == GameMode::getCols())
-                        numNeighbors += 2;
-                    else
-                        ++numNeighbors;
-                }
-            }
-        }
     }
 
-    else if (row == GameMode::getRows())
+    else if (col == COLS - 1)
     {
-        numNeighbors += 2;
-        for (int i = row - 1; i < row + 1; ++i)
+      for (int r = 0; r < 2; ++r)
+      {
+        for (int c = getCols() - 2; c < COLS; ++c)
         {
-            for (int j = col; j < col + 2; ++j)
-            {
-                if (i == row && j == col)
-                    continue;
-                else if (GameMode::isOccupied(thisGen[i][j]))
-                {
-                    if (i == 0 || j == 0 || i == GameMode::getRows() || j == GameMode::getCols())
-                        numNeighbors += 2;
-                    else
-                        ++numNeighbors;
-                }
-            }
+          if (r == row && c == col)
+            numNeighbors += 3;
+          else if (GameMode::isOccupied(thisGen[r][c]))
+          {
+            if (r == 0 || c == COLS - 1 || r == ROWS - 1 || c == 0)
+              numNeighbors += 2;
+            else
+              ++numNeighbors;
+          }
         }
+      }
     }
 
     else
     {
-        ++numNeighbors;
-        for (int i = row - 1; i < row + 2; ++i)
+      for (int r = 0; r < 2; ++r)
+      {
+        for (int c = col - 1; c < col + 2; ++c)
         {
-            for (int j = col; j < col + 2; ++j)
-            {
-                if (i == row && j == col)
-                    continue;
-                else if (GameMode::isOccupied(thisGen[i][j]))
-                {
-                    if (i == 0 || j == 0 || i == GameMode::getRows() || j == GameMode::getCols())
-                        numNeighbors += 2;
-                    else
-                        ++numNeighbors;
-                }
-            }
+          if (r == row && c == col)
+            numNeighbors++;
+          else if (GameMode::isOccupied(thisGen[r][c]))
+          {
+            if (r == 0 || c == COLS - 1 || r == ROWS - 1 || c == 0)
+              numNeighbors += 2;
+            else
+              ++numNeighbors;
+          }
         }
+      }
     }
-}//end col == 0
+  }
 
-else if (col == GameMode::getCols())
-{
-    if (row == 0)
+  else if (row == ROWS - 1)
+  {
+    if (col == 0)
     {
-        numNeighbors += 2;
-        for (int i = row; i < row + 2; ++i)
+      for (int r = ROWS - 2; r < ROWS; ++r)
+      {
+        for (int c = 0; c < 2; ++c)
         {
-            for (int j = col - 1; j < col + 1; ++j)
-            {
-                if (i == row && j == col)
-                    continue;
-                else if (GameMode::isOccupied(thisGen[i][j]))
-                {
-                    if (i == 0 || j == 0 || i == GameMode::getRows() || j == GameMode::getCols())
-                        numNeighbors += 2;
-                    else
-                        ++numNeighbors;
-                }
-            }
+          if (r == row && c == col)
+            numNeighbors += 3;
+          else if (GameMode::isOccupied(thisGen[r][c]))
+          {
+            if (r == 0 || c == COLS - 1 || r == ROWS - 1 || c == 0)
+              numNeighbors += 2;
+            else
+              ++numNeighbors;
+          }
         }
+      }
     }
 
-    else if (row == GameMode::getRows())
+    else if (col == COLS - 1)
     {
-        numNeighbors += 2;
-        for (int i = row - 1; i < row + 1; ++i)
+      for (int r = ROWS - 2; r < ROWS; ++r)
+      {
+        for (int c = COLS - 2; c < COLS; ++c)
         {
-            for (int j = col - 1; j < col + 1; ++j)
-            {
-                if (i == row && j == col)
-                    continue;
-                else if (GameMode::isOccupied(thisGen[i][j]))
-                {
-                    if (i == 0 || j == 0 || i == GameMode::getRows() || j == GameMode::getCols())
-                        numNeighbors += 2;
-                    else
-                        ++numNeighbors;
-                }
-            }
+          if (r == row && c == col)
+            numNeighbors += 3;
+          else if (GameMode::isOccupied(thisGen[r][c]))
+          {
+            if (r == 0 || c == COLS - 1 || r == ROWS - 1 || c == 0)
+              numNeighbors += 2;
+            else
+              ++numNeighbors;
+          }
         }
+      }
     }
 
     else
     {
-        ++numNeighbors;
-        for (int i = row - 1; i < row + 2; ++i)
+      for (int r = ROWS - 2; r < ROWS; ++r)
+      {
+        for (int c = col - 1; c < col + 2; ++c)
         {
-            for (int j = col - 1; j < col + 1; ++j)
-            {
-                if (i == row && j == col)
-                    continue;
-                else if (GameMode::isOccupied(thisGen[i][j]))
-                {
-                    if (i == 0 || j == 0 || i == GameMode::getRows() || j == GameMode::getCols())
-                        numNeighbors += 2;
-                    else
-                        ++numNeighbors;
-                }
-            }
+          if (r == row && c == col)
+            numNeighbors++;
+          else if (GameMode::isOccupied(thisGen[r][c]))
+          {
+            if (r == 0 || c == COLS - 1 || r == ROWS - 1 || c == 0)
+              numNeighbors += 2;
+            else
+              ++numNeighbors;
+          }
         }
+      }
     }
-}//end col==GameMode::getCols()
-return numNeighbors;
+  }
+
+  else if (col == 0)
+  {
+    if (row == 0)
+    {
+      for (int r = 0; r < 2; ++r)
+      {
+        for (int c = 0; c < 2; ++c)
+        {
+          if (r == row && c == col)
+            numNeighbors += 3;
+          else if (GameMode::isOccupied(thisGen[r][c]))
+          {
+            if (r == 0 || c == COLS - 1 || r == ROWS - 1 || c == 0)
+              numNeighbors += 2;
+            else
+              ++numNeighbors;
+          }
+        }
+      }
+    }
+
+    else if (row == ROWS - 1)
+    {
+      for (int r = ROWS - 2; r < ROWS; ++r)
+      {
+        for (int c = 0; c < 2; ++c)
+        {
+          if (r == row && c == col)
+            numNeighbors += 3;
+          else if (GameMode::isOccupied(thisGen[r][c]))
+          {
+            if (r == 0 || c == COLS - 1 || r == ROWS - 1 || c == 0)
+              numNeighbors += 2;
+            else
+              ++numNeighbors;
+          }
+        }
+      }
+    }
+
+    else
+    {
+      for (int r = row - 1; r < row + 2; ++r)
+      {
+        for (int c = 0; c < 2; ++c)
+        {
+          if (r == row && c == col)
+            numNeighbors++;
+          else if (GameMode::isOccupied(thisGen[r][c]))
+          {
+            if (r == 0 || c == COLS - 1 || r == ROWS - 1 || c == 0)
+              numNeighbors += 2;
+            else
+              ++numNeighbors;
+          }
+        }
+      }
+    }
+  }
+
+  else if (col == COLS - 1)
+  {
+    if (row == 0)
+    {
+      for (int r = 0; r < 2; ++r)
+      {
+        for (int c = COLS - 2; c < COLS; ++c)
+        {
+          if (r == row && c == col)
+            numNeighbors++;
+          else if (GameMode::isOccupied(thisGen[r][c]))
+          {
+            if (r == 0 || c == COLS - 1 || r == ROWS - 1 || c == 0)
+              numNeighbors += 2;
+            else
+              ++numNeighbors;
+          }
+        }
+      }
+    }
+
+    else if (row == ROWS - 1)
+    {
+      for (int r = ROWS - 2; r < ROWS; ++r)
+      {
+        for (int c = COLS - 2; c < COLS; ++c)
+        {
+          if (r == row && c == col)
+            numNeighbors++;
+          else if (GameMode::isOccupied(thisGen[r][c]))
+          {
+            if (r == 0 || c == COLS - 1 || r == ROWS - 1 || c == 0)
+              numNeighbors += 2;
+            else
+              ++numNeighbors;
+          }
+        }
+      }
+    }
+
+    else
+    {
+      for (int r = row - 1; r < row + 2; ++r)
+      {
+        for (int c = COLS - 2; c < COLS; ++c)
+        {
+          if (r == 0 || c == COLS - 1 || r == ROWS - 1 || c == 0)
+            numNeighbors += 2;
+          else
+            ++numNeighbors;
+        }
+      }
+    }
+  }
+
+  else
+  {
+    for (int r = row - 1; r < row + 2; ++r)
+    {
+       for (int c = col - 1; c < col + 2; ++c)
+       {
+          if (r == row && c == col)
+             continue;
+          else if (GameMode::isOccupied(thisGen[r][c]))
+             ++numNeighbors;
+       }
+    }
+  }
+  return numNeighbors;
 }
